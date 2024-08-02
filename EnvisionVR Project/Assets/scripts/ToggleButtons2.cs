0a23cc2328fc041a94b39e48ef22c4354afe4e97
SVKambhampati <shreyas.venkatasai@gmail.com>
2024-08-02 11:28:20 -0500

    Final

---
+++ EnvisionVR Project/Assets/scripts/ToggleButtons2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleButtons2 : MonoBehaviour
+{
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void ToggleGameObj()
+    {
+        obj.SetActive(!obj.activeSelf);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
